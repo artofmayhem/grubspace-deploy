8ac9b60c4e77a64d7d3ba4d4f4d9e21128b169d8
artofmayhem <chef_tony_miller@yahoo.com>
2021-08-20 11:39:15 -1000

    swiper added and working chef page

--- src/components/Chef.js
+++ src/components/Chef.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 import ScrollToTop from "react-scroll-to-top";
 import { CookingShow1Img, SurLaTableImg } from "../assets";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import YouTube from "react-youtube";
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
 const initialState =
   "Cherries, apricots, plums, pears, apples, quinces, peaches, strawberries, and blackberries are all part of the rose family. These types of fruits typically have flowers with five equal petals arranged around a central core. Unlike many other fruits, once picked, unripe raspberries do not ripen.";
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 export default function Chef() {
   const [trivia, setTrivia] = useState(initialState);
@@ -18,9 +28,9 @@ export default function Chef() {
       "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     },
   };
-
   const youtubeOptions = {
-    width: 350,
+    width: 640,
+    height: 360,
     playerVars: {
       rel: 0,
       showinfo: 0,
@@ -51,7 +61,11 @@ export default function Chef() {
           "chef-container bg-black  w-screen flex flex-col justify-center align-center"
         }
       >
-        <h2 className={"animate-fade-in-down text-white mx-auto -mt-10 text-5xl"}>The Chef Spot</h2>
+        <h2
+          className={"animate-fade-in-down text-white mx-auto -mt-10 text-5xl"}
+        >
+          The Chef Spot
+        </h2>
       </div>
       <div
         className={
@@ -90,7 +104,9 @@ export default function Chef() {
             className="flex flex-row flex-wrap justify-evenly bg-gray-800 border-4 text-center"
             style={{ width: 375 }}
           >
-            <h2 className={"mx-auto my-auto py-10 text-5xl"}>Food Fun Facts!</h2>
+            <h2 className={"mx-auto my-auto py-10 text-5xl"}>
+              Food Fun Facts!
+            </h2>
             <button
               className={
                 "animate-bounce px-20 py-6 bg-gray-400 h-20 text-white text-2xl mx-auto"
@@ -151,162 +167,133 @@ export default function Chef() {
             </div>
           </a>
         </div>
-        <div className="h-auto w-screen flex flex-col flex-wrap justify-center pt-16 bg-gray-600">
+        <div className="h-auto w-screen flex flex-col flex-wrap justify-center pt-16 pb-48 bg-gray-600">
           <div className={"flex justify-center text-5xl mt-10  text-white"}>
-            <h3>Latest Videos</h3>
+            <h3>Video Gallery</h3>
           </div>
           <hr
             className="my-16 self-center"
             style={{ minWidth: 300, width: "60vw" }}
           />
-          <div
-            className={"h-auto flex flex-row flex-wrap justify-evenly pt-16"}
+          <Swiper
+            spaceBetween={300}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+            className={"py-10 bg-yellow-600 border-2 text-white"}
+            style={{ maxWidth: "90vw", minWidth: "350px" }}
           >
-            <div
-              className="flex flex-col flex-wrap flex-center my-auto py-10 px-4 text-white bg-yellow-600 text-center"
-              style={{ width: 350 }}
-            >
-              <h3 className={"text-3xl my-2"}><strong>Basics with Babish</strong></h3>
-              <h3 className={"text-xl my-2"}><strong>Babish Culinary University</strong></h3>
-              <h3 className={"text-xl my-2"}>Episode: Chopped Cheese</h3>
-              <h3 className={"text-xl my-4"}>
-                Join Babish as he throws down on a delicious New York City
-                landmark, the ubiquitous chopped cheese.
-              </h3>
-              <a
-                href={"https://basicswithbabish.co/basicsepisodes/date-night"}
-                className={
-                  "text-xl my-2 text-gray-800 border-2 py-2 bg-gray-400"
-                }
-              >
-                Get the Recipe
-              </a>
-            </div>
-            <div className="flex justify-center align-center mb-24 " style={{ width: "50%", paddingTop: '5rem' }}>
-              <YouTube opts={youtubeOptions} videoId="E1z55dYHv8c" />
-            </div>
-          </div>
-          <div className={"h-auto flex flex-row flex-wrap justify-evenly"}>
-            <div
-              className="flex flex-col flex-wrap flex-center my-auto py-10 px-4 text-white bg-yellow-600 text-center"
-              style={{ width: 350 }}
-            >
-              <h3 className={"text-3xl my-2"}><strong>Kenji Lopez-Alt</strong></h3>
-              <h3 className={"text-xl my-2"}><strong>Kenji's Cooking Show</strong></h3>
-              <h3 className={"text-xl my-2"}>Episode: Black Bean Burgers</h3>
-              <h3 className={"text-xl my-4"}>
-                Get your plant on with Kenji as he shares his sure fire,
-                showstopping black bean burger recipe.
-              </h3>
-              <a
-                href={
-                  "https://www.seriouseats.com/the-best-black-bean-burger-recipe"
-                }
-                className={
-                  "text-xl my-2 text-gray-800 border-2 py-2 bg-gray-400"
-                }
-              >
-                Get the Recipe
-              </a>
-            </div>
-            <div
-              className="flex justify-center align-center mb-24 "
-              style={{ width: "50%", paddingTop: '5rem' }}
-            >
-              <YouTube opts={youtubeOptions} videoId="BMgLRD2v5w0" />
-            </div>
-          </div>
-          <div className={"h-auto flex flex-row flex-wrap justify-evenly"}>
-            <div
-              className="flex flex-col flex-wrap flex-center my-auto py-10 px-4 text-white bg-yellow-600 text-center"
-              style={{ width: 350 }}
-            >
-              <h3 className={"text-3xl my-2"}><strong>Marcus Meacham</strong></h3>
-              <h3 className={"text-xl my-2"}><strong>Tastemade</strong></h3>
-              <h3 className={"text-xl my-2"}>
-                Episode: Ramen Fried Chicken Sandwich
-              </h3>
-              <h3 className={"text-xl my-4"}>
-                Marcus makes his version of southern fried chicken sandwiches
-                and adds an epic twist: he batters the chicken thighs in crushed
-                Ramen.
-              </h3>
-              <a
-                href={
-                  "https://www.youtube.com/playlist?list=PLX98sAmndWt0QIK3nBvV4QWS5g8zlbhT_"
-                }
-                className={
-                  "text-xl my-2 text-gray-800 border-2 py-2 bg-gray-400"
-                }
-              >
-                Get More Recipes
-              </a>
-            </div>
-            <div
-              className="flex justify-center mb-24 "
-              style={{ width: "50%", paddingTop: '5rem' }}
-            >
-              <YouTube opts={youtubeOptions} videoId="mCJUcJJUmvg" />
-            </div>
-          </div>
-          <div className={"h-auto flex flex-row flex-wrap justify-evenly py-20"}>
-            <div
-              className="flex flex-col flex-wrap flex-center  py-10 px-4 text-white bg-yellow-600 text-center"
-              style={{ width: 350 }}
-            >
-              <h3 className={"text-3xl my-2"}><strong>Niki Shariley</strong></h3>
-              <h3 className={"text-xl my-2"}><strong>UrbanBushBabes</strong></h3>
-              <h3 className={"text-xl my-2"}>Episode: Rice and Peas</h3>
-              <h3 className={"text-xl my-4"}>
-                Step into the fire with Niki as she breaks down the secrets to her amped up take on a Caribbean classic, Coconut Rice and Beans.
-              </h3>
-              <a
-                href={
-                  "https://www.pinterest.com/pin/108227197274383070/"
-                }
-                className={
-                  "text-xl my-2 text-gray-800 border-2 py-2 bg-gray-400"
-                }
-              >
-                Get the Recipe
-              </a>
-            </div>
-            <div
-              className="flex justify-center mb-24 "
-              style={{ width: "50%", paddingTop: '5rem' }}
-            >
-              <YouTube opts={youtubeOptions} videoId="-65DoHCZGdQ" />
-            </div>
-          </div>
-          <div className={"h-auto flex flex-row flex-wrap justify-evenly items-end mb-20"}>
-            <div
-              className="flex flex-col flex-wrap flex-center my-auto py-10 px-4 text-white bg-yellow-600 text-center"
-              style={{ width: 350 }}
-            >
-              <h3 className={"text-3xl my-2"}><strong>Joshua Weissman</strong></h3>
-              <h3 className={"text-xl my-2"}><strong>Youtuber</strong></h3>
-              <h3 className={"text-xl my-2"}>Episode: The $2 Sandwich</h3>
-              <h3 className={"text-xl my-4"}>
-              A great sandwich is usually overpriced. But you can make this homemade sandwich for a total of 2 dollars PER SERVING. That means your 12 dollar sandwich is going right out the window. 
-              </h3>
-              <a
-                href={
-                  "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
-                }
-                className={
-                  "text-xl my-2 text-gray-800 border-2 py-2 bg-gray-400"
-                }
-              >
-                Get the Recipe
-              </a>
-            </div>
-            <div
-              className="flex justify-center mb-24 "
-              style={{ width: "50%", paddingTop: '5rem' }}
-            >
-              <YouTube opts={youtubeOptions} videoId="gjhOPHoRJBs" />
-            </div>
-          </div>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube opts={youtubeOptions} videoId="E1z55dYHv8c" />{" "}
+                <a
+                  href={"https://basicswithbabish.co/basicsepisodes/date-night"}
+                >
+                  Click here for the best of Basics With Babish
+                </a>
+              </p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube
+                  name={"weissman"}
+                  opts={youtubeOptions}
+                  videoId="gjhOPHoRJBs"
+                />{" "}
+                <a
+                  href={
+                    "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
+                  }
+                >
+                  Click here for Joshua Weissman's recipe
+                </a>
+              </p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube opts={youtubeOptions} videoId="-65DoHCZGdQ" />{" "}
+                <a
+                  href={
+                    "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
+                  }
+                >
+                  Explore the tantalizing flavors of Nikisha Riley
+                </a>
+              </p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube opts={youtubeOptions} videoId="mCJUcJJUmvg" />{" "}
+                <a
+                  href={
+                    "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
+                  }
+                >
+                  Get Creative with Chef Meach of Tastemade
+                </a>
+              </p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube opts={youtubeOptions} videoId="BMgLRD2v5w0" />{" "}
+                <a
+                  href={
+                    "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
+                  }
+                >
+                  Experience the flavors and charm of Kenji Lopez-Alt
+                </a>
+              </p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube opts={youtubeOptions} videoId="UbNiP8ilvCc" />{" "}
+                <a
+                  href={
+                    "https://www.youtube.com/channel/UCSX8k4RapYLGEO5G_y_M37Q"
+                  }
+                >
+                  Try something new with the ladies of Natural Life TV Cooking
+                </a>
+              </p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube opts={youtubeOptions} videoId="E8b7PwchlIg" />{" "}
+                <a
+                  href={
+                    "https://kentrollins.com/homemade-candied-jalapeno-bacon/"
+                  }
+                >
+                  Experience the open trails with Cowboy Kent Rollins
+                </a>
+              </p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube opts={youtubeOptions} videoId="nW_Tpken5cg" />{" "}
+                <a
+                  href={
+                    "https://www.youtube.com/channel/UCSX8k4RapYLGEO5G_y_M37Q"
+                  }
+                >
+                  Re-live the classics with Julia Child
+                </a>
+              </p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p className={"text-2xl"}>
+                <YouTube opts={youtubeOptions} videoId="7IZeh7w5ekY" />{" "}
+                <a href={"https://www.almazankitchen.com/bacon-eggs-pan-pie/"}>
+                  Feeling adventurous? Go bushcrafting with Almazan Kitchen
+                </a>
+              </p>
+            </SwiperSlide>
+            ...
+          </Swiper>
         </div>
       </div>
     </React.Fragment>
--- src/components/Nutrition.js
+++ src/components/Nutrition.js
@@ -2,21 +2,22 @@ import React from "react";
 import axios from "axios";
 import ScrollToTop from "react-scroll-to-top";
 // import Swiper core and required modules
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import YouTube from "react-youtube";
 
 // Import Swiper styles
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
 // install Swiper modules
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const youtubeOptions = {
-  width: 350,
+  width: 640,
+  height: 360,
   playerVars: {
     rel: 0,
     showinfo: 0,
@@ -27,34 +28,33 @@ const youtubeOptions = {
 };
 
 export default function Nutrition() {
-
-
   const options = {
-    method: 'GET',
-    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate',
+    method: "GET",
+    url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate",
     params: {
-      timeFrame: 'day', // or week
-      targetCalories: '2500', //input a number
-      diet: 'Paleo', // Gluten Free, Ketogenic, Vegetarian, Lacto-Vegetarian, Ovo-Vegetarian, Vegan, Pescetarian, Paleo, Whole30, Omnivore
-      exclude: 'chicken' // input a string with words separated by comma
+      timeFrame: "day", // or week
+      targetCalories: "2500", //input a number
+      diet: "Paleo", // Gluten Free, Ketogenic, Vegetarian, Lacto-Vegetarian, Ovo-Vegetarian, Vegan, Pescetarian, Paleo, Whole30, Omnivore
+      exclude: "chicken", // input a string with words separated by comma
     },
     headers: {
-      'x-rapidapi-key': 'cc687eba84mshcc7485fcf110baap193a15jsnfb1be463a74d',
-      'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-    }
+      "x-rapidapi-key": "cc687eba84mshcc7485fcf110baap193a15jsnfb1be463a74d",
+      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    },
   };
-  
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
 
+  // axios
+  //   .request(options)
+  //   .then(function (response) {
+  //     console.log(response.data);
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //   });
 
   return (
     <React.Fragment>
-       <ScrollToTop smooth />
+      <ScrollToTop smooth />
       <div
         className={
           "nutrition-container bg-black text-center w-screen flex flex-col justify-center align-center"
@@ -70,27 +70,131 @@ export default function Nutrition() {
       </div>
       <div
         className={
-          "bg-white h-96 w-screen flex flex-col justify-center text-center align-center"
+          "bg-gray-600 py-48 w-screen my-auto mx-auto flex flex-col justify-center text-center align-center"
         }
+        style={{ height: "100%", maxWidth: "100vw" }}
       >
         <Swiper
-      spaceBetween={300}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide >
-      <YouTube opts={youtubeOptions} videoId="E1z55dYHv8c" />
-      </SwiperSlide>
-      <SwiperSlide> <YouTube opts={youtubeOptions} videoId="gjhOPHoRJBs" /></SwiperSlide>
-      <SwiperSlide> <YouTube opts={youtubeOptions} videoId="-65DoHCZGdQ" /></SwiperSlide>
-      <SwiperSlide>  <YouTube opts={youtubeOptions} videoId="mCJUcJJUmvg" /></SwiperSlide>
-      <SwiperSlide>  <YouTube opts={youtubeOptions} videoId="BMgLRD2v5w0" /></SwiperSlide>
-      ...
-    </Swiper>
+          spaceBetween={300}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+          className={"py-10 bg-yellow-600 border-2 text-white"}
+          style={{ maxWidth: "90vw", minWidth: "350px" }}
+        >
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube opts={youtubeOptions} videoId="E1z55dYHv8c" />{" "}
+              <a href={"https://basicswithbabish.co/basicsepisodes/date-night"}>
+                Click here for the best of Basics With Babish
+              </a>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube
+                name={"weissman"}
+                opts={youtubeOptions}
+                videoId="gjhOPHoRJBs"
+              />{" "}
+              <a
+                href={
+                  "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
+                }
+              >
+                Click here for Joshua Weissman's recipe
+              </a>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube opts={youtubeOptions} videoId="-65DoHCZGdQ" />{" "}
+              <a
+                href={
+                  "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
+                }
+              >
+                Explore the tantalizing flavors of Nikisha Riley
+              </a>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube opts={youtubeOptions} videoId="mCJUcJJUmvg" />{" "}
+              <a
+                href={
+                  "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
+                }
+              >
+                Get Creative with Chef Meach of Tastemade
+              </a>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube opts={youtubeOptions} videoId="BMgLRD2v5w0" />{" "}
+              <a
+                href={
+                  "https://www.joshuaweissman.com/post/2-dollar-sandwich-but-cheaper"
+                }
+              >
+                Experience the flavors and charm of Kenji Lopez-Alt
+              </a>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube opts={youtubeOptions} videoId="UbNiP8ilvCc" />{" "}
+              <a
+                href={
+                  "https://www.youtube.com/channel/UCSX8k4RapYLGEO5G_y_M37Q"
+                }
+              >
+                Try something new with the ladies of Natural Life TV Cooking
+              </a>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube opts={youtubeOptions} videoId="E8b7PwchlIg" />{" "}
+              <a
+                href={
+                  "https://kentrollins.com/homemade-candied-jalapeno-bacon/"
+                }
+              >
+                Experience the open trails with Cowboy Kent Rollins
+              </a>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube opts={youtubeOptions} videoId="nW_Tpken5cg" />{" "}
+              <a
+                href={
+                  "https://www.youtube.com/channel/UCSX8k4RapYLGEO5G_y_M37Q"
+                }
+              >
+               Re-live the classics with Julia Child
+              </a>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={"text-2xl"}>
+              <YouTube opts={youtubeOptions} videoId="7IZeh7w5ekY" />{" "}
+              <a
+                href={
+                  "https://www.almazankitchen.com/bacon-eggs-pan-pie/"
+                }
+              >
+                Feeling adventurous? Go bushcrafting with Almazan Kitchen
+              </a>
+            </p>
+          </SwiperSlide>
+          ...
+        </Swiper>
       </div>
     </React.Fragment>
   );

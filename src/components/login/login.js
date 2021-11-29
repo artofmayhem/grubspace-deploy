9eba3b524eeaaf3dc3ec991e74ff59b29855c026
artofmayhem <chef_tony_miller@yahoo.com>
2021-11-29 08:27:16 -1000

    changes

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,26 +1,24 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Button, LinearProgress, TextField } from "@material-ui/core";
+import { LinearProgress } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import API from "../../api/api";
+// import API from "../../api/api";
 import signUpSchema from "./signUpSchema";
 import loginSchema from "./loginSchema";
 import initialFormErrors from "../../state/initial-states/initialFormErrors";
-import initialCredentials from "../../state/initial-states/initialCredentials";
-import { Parallax } from "react-parallax";
+import initialCredentials from '../../state/initial-states/initialCredentials';
+// import { Parallax } from "react-parallax";
 import * as yup from "yup";
-
-import { FamilyDinner } from "../../assets/index";
-
-const LoginForm = () => {
+// import { postLogIn, createUser } from "../../state/actionCreators";
+// import { FamilyDinner } from "../../assets/index";
+import { connect } from "react-redux";
+const LoginForm = (props) => {
   const history = useHistory();
-
   const [user, setUser] = useState(initialCredentials);
   const [login, setLogin] = useState(true);
   const [fetching, setFetching] = useState(false);
   const [formErrors, setErrors] = useState(initialFormErrors);
   const [disabled, setDisabled] = useState(true);
-  const [backendError, setBackendError] = useState();
 
   //helper functions
 
@@ -29,13 +27,13 @@ const LoginForm = () => {
     //allows react to keep the event object to play nicely with the async
     e.persist();
     if (login) {
-      console.log(loginSchema, "login");
+      // console.log(loginSchema, "login");
       yup
         .reach(loginSchema, e.target.name)
         .validate(e.target.name)
         .then((valid) => {
           //logs validation truthiness
-          console.log(valid);
+          // console.log(valid);
           setErrors({
             ...formErrors,
             [e.target.name]: "",
@@ -46,14 +44,14 @@ const LoginForm = () => {
           console.log({ error });
         });
     } else {
-      console.log(signUpSchema, "signup");
+      // console.log(signUpSchema, "signup");
       yup
         .reach(signUpSchema, e.target.name)
 
         .validate(e.target.name)
         .then((valid) => {
           //logs validation truthiness
-          console.log(valid);
+          // console.log(valid);
           setErrors({
             ...formErrors,
             [e.target.name]: "",
@@ -84,131 +82,123 @@ const LoginForm = () => {
 
     setFetching(true);
     if (login) {
-      API.post("", user)
-        .then(({ data }) => {
-          localStorage.setItem("token", data.token);
-          localStorage.setItem("user", JSON.stringify(data.user));
-          setUser(initialCredentials);
+      console.log("user from login", user);
+      props.postLogIn(user,(isSuccessful)=>{
+        if(isSuccessful){
           history.push("/userrecipes");
-          setFetching(false);
-        })
-        .catch((err) => {
-          alert(
-            "Please Provide a Valid Username, and Password Combination or Start A New Account By Clicking The Sign-up Button"
-          );
-          const backError = err.response.data.message;
-          setBackendError(backError);
-          console.log(backError, "sign in error from the api");
-        });
+        }
+      });
     } else {
-      API.post("", user)
-        .then(({ data }) => {
+      console.log("user from register", user);
+      props.createUser(user,(isSuccessful)=>{
+        if(isSuccessful){
           alert("Account Created! Please Login to Continue");
           setLogin(true);
           setFetching(false);
           history.push("/login");
-        })
-        .catch((err) => {
-          alert(
-            "Please Provide a Valid Username, Email, and Password (6-15 characters long) to Create an Account"
-          );
-          const backError = err.response.data.message;
-          setBackendError(backError);
-          console.log(backError, "sign in error from the api");
-        });
+        }
+      });
     }
   };
+  // console.log("user data", userData );
+
 
   return (
-    <Parallax bgImage={FamilyDinner} strength={500}>
+
       <div
-        className={"flex flex-col justify-center items-center"}
-        style={{ height: "75vh" }}
+        className={" login-container flex flex-col justify-center items-center py-10 "}
+        style={{ height: "100vh", width: '100vw'}}
       >
-        <Form onSubmit={onSubmit}>
+        <Form onSubmit={onSubmit} style={{backgroundColor: '#222', opacity: 0.9, color: 'white', width: '35vw', minWidth: 370, margin: '5rem 0'}}>
           <h2
-            className={"text-white text-4xl"}
+            className={"text-white text-4xl mb-4 "}
             style={{ textShadow: "0 0 1rem black", fontWeight: 400 }}
           >
             {login ? "Login" : "Sign Up"}
           </h2>
-          <TextField
-            required
-            type="text"
-            name="username"
-            value={user.username}
+          <h6 className={'my-1'}>User Name</h6>
+          <input
+              className={
+                "animate-fade-in-1 border-solid border-2 border-white p-2 w-60 mx-auto my-1 text-center text-black"
+              }
+              type="text"
+            name="user_username"
+            value={user.user_username}
             onChange={handleChange}
             label="username"
-            margin="dense"
-            style={{ width: 300 }}
-          />
-          {!login ? (
-            <TextField
-              required
-              name="email"
-              type="email"
-              value={user.email}
-              onChange={handleChange}
-              // variant="outlined"
-              label="email"
-              margin="dense"
-              style={{ width: 300 }}
+              style={{ boxShadow: "0 0 1.5rem #444" }}
+            placeholder="User Name"
             />
+        
+          {!login ? (
+            <>
+            <h6 className={'my-1'}>Email</h6>
+            <input
+            className={
+              "animate-fade-in-1 border-solid border-2 border-white p-2 w-60 mx-auto my-1 text-center text-black"
+            }
+            name="user_email"
+            type="email"
+          value={user.user_email}
+          onChange={handleChange}
+          label="Email"
+            style={{ boxShadow: "0 0 1.5rem #444" }}
+          placeholder="User Email"
+          />
+          </>
           ) : (
             <></>
           )}
-          <TextField
-            required
-            name="password"
+          <h6 className={'my-1'}>Password</h6>
+          <input
+            className={
+              "animate-fade-in-1 border-solid border-2 border-white p-2 w-60 mx-auto my-1 text-center text-black"
+            }
+            name="user_password"
             type="password"
-            value={user.password}
+            value={user.user_password}
             onChange={handleChange}
+            placeholder="Password"
             label="password"
-            margin="dense"
-            style={{ width: 300 }}
+            style={{ boxShadow: "0 0 1.5rem #444" }}
           />
-          <h6 style={{ textAlign: "center", color: "red" }}>{backendError}</h6>
+          {/* <h6 style={{ textAlign: "center", color: "red" }}>{backendError}</h6> */}
           <button
             disabled={disabled}
             type="submit"
-            style={{
-              color: "white",
-              border: "1px solid white",
-              width: 300,
-              height: 40,
-              marginTop: 20,
-              background: "#444",
-              borderRadius: 50,
-              fontWeight: "bold",
-            }}
+            className={
+              "animate-fade-in-2 border-2 bg-gray-400 text-white px-10 py-4 m-auto w-60"
+            }
           >
             {login ? "Login" : "Sign Up"}
           </button>
           {fetching ? <LinearProgress color="primary" /> : <></>}
         </Form>
-        <Button
-          size="small"
-          variant="contained"
-          style={{
-            width: 300,
-            height: 50,
-            borderRadius: 50,
-            marginTop: 50,
-            background: "#444",
-            color: "white",
-            fontWeight: "bold",
-            border: "1px solid white",
-          }}
+        <button
+
+          className={
+            "animate-fade-in-2 border-2 bg-gray-400 text-white px-10 py-4 mt-6 mb-10 w-60"
+          }
           onClick={() => setLogin(!login)}
         >
           {login ? "Go to Sign Up" : "Go to Log In"}
-        </Button>
+        </button>
       </div>
-    </Parallax>
   );
 };
 
-export default LoginForm;
+const mapStateToProps = (state) => ({
+  // status: state.api.postLogIn.status,
+  // error: state.api.postLogIn.errMsg
+});
+
+const mapDispatchToProps = {
+  // postLogIn,
+  // createUser
+};
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(LoginForm);
 
 const Form = styled.form`
   display: flex;
--- src/components/login/loginSchema.js
+++ src/components/login/loginSchema.js
@@ -1,11 +1,11 @@
 import * as yup from "yup";
 
 const loginSchema = yup.object().shape({
-  username: yup
+  user_username: yup
     .string()
     .min(2, "username must be at least two characters long")
     .required("Please provide a username"),
-  password: yup
+  user_password: yup
     .string()
     .min(6, "please choose a password between 6-15 characters long")
     .max(15, "please choose a password between 6-15 characters long")
